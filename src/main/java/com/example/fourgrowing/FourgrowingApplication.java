ab05c749224dafb37053ad98bb0effd42d5292fa
BeomJoon <faulty337@gmail.com>
2022-08-29 18:16:32 +0900

    스프링부트 초기 세팅

---
+++ src/main/java/com/example/fourgrowing/FourgrowingApplication.java
@@ -0,0 +1,13 @@
+package com.example.fourgrowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourgrowingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourgrowingApplication.class, args);
+    }
+
+}
