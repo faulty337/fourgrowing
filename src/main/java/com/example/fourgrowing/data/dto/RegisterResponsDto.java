320fb80df6ab48302929abb4305ef3d8b00289dd
BeomJoon <faulty337@gmail.com>
2022-09-13 17:57:48 +0900

    Product 등록 예외처리

--- src/main/java/com/example/fourgrowing/data/dto/RegisterResponsDto.java
+++ src/main/java/com/example/fourgrowing/data/dto/RegisterResponsDto.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RegisterResponsDto {
-    private boolean result;
-
+    private String result;
+    
     private String errorCode;
 }
--- src/main/java/com/example/fourgrowing/service/ProductService.java
+++ src/main/java/com/example/fourgrowing/service/ProductService.java
@@ -2,11 +2,13 @@ package com.example.fourgrowing.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 import com.example.fourgrowing.data.entity.Product;
 import com.example.fourgrowing.data.dto.ProductCreateDto;
 import com.example.fourgrowing.data.dto.ProductResponseDto;
+import com.example.fourgrowing.data.dto.RegisterResponsDto;
 import com.example.fourgrowing.repository.ProductRepository;
 import com.google.common.base.CharMatcher;
 
@@ -54,17 +56,49 @@ public class ProductService {
 		return result;
 	}
 
-	public Product setUsername(String name, String plantCode, String plantname) {
-		
-		String strBase64Encode = new String(Base64Utils.decode((plantCode + "=").getBytes()));
+	public RegisterResponsDto setUsername(String name, String plantCode, String plantname) {
+		String strBase64Encode = "";
+		RegisterResponsDto registerResponsDto = new RegisterResponsDto();
 		String charsToRetain = "0123456789"; 
-		Long productId = Long.parseLong(CharMatcher.anyOf(charsToRetain).retainFrom(strBase64Encode));
-		Product product = productRepository.findById(productId).get();
+		Long productId = new Long(1);
+		try{
+			strBase64Encode = new String(Base64Utils.decode((plantCode + "=").getBytes()));
+		}catch(IllegalArgumentException e){
+			registerResponsDto.setErrorCode("잘못된 코드");
+			registerResponsDto.setResult("false");
+			return registerResponsDto;
+		}
+		try {
+			productId = Long.parseLong(CharMatcher.anyOf(charsToRetain).retainFrom(strBase64Encode));
+		} catch (NumberFormatException e) {
+			registerResponsDto.setErrorCode("없는 코드");
+			registerResponsDto.setResult("false");
+			return registerResponsDto;
+		}
+		Product product = new Product();
+		try {
+			product = productRepository.findById(productId).get();
+		} catch (NoSuchElementException e) {
+			registerResponsDto.setErrorCode("없는 코드");
+			registerResponsDto.setResult("false");
+			return registerResponsDto;
+		}
+
+		if(product.getUsername() != null){
+			registerResponsDto.setErrorCode("이미 등록된 코드");
+			registerResponsDto.setResult("false");
+			return registerResponsDto;
+		}
+		
+		
 		product.setUsername(name);
 		product.setPlantname(plantname);
 		product = productRepository.save(product);
-		return product;
+		
+		registerResponsDto.setResult("true");
+		return registerResponsDto;
 	}
+
 
 	// public RegistrationResponsDto registrationUser(String username, String ProductCode){
 	// 	RegistrationResponsDto registrationResponsDto = new RegistrationResponsDto();
